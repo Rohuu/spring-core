5ec4b3630425bdd14e8b3b689f9d5ce4efbca7a1
Rohuu <rohuu99022@gmail.com>
2023-03-21 16:00:57 +0530

    used application context to instantiate class

--- SpringDemo/src/com/rohuu/drawingApp/DrawingApp.java
+++ SpringDemo/src/com/rohuu/drawingApp/DrawingApp.java
@@ -2,6 +2,8 @@ package com.rohuu.drawingApp;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
@@ -11,8 +13,12 @@ public class DrawingApp {
 		
 		// using bean factory to instantiate the Triangle class
 //		Triangle triangle= new Triangle();
-		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle= (Triangle) factory.getBean("triangle");
+//		BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		
+		// using application context instead of bean factory
+		// in ApplicationContext we don't have to use FileSystemResource object
+		ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle= (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 }
