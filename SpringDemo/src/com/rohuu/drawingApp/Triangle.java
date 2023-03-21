8e2636e36437982a52384b14c1b50e771ee984da
Rohuu <rohuu99022@gmail.com>
2023-03-21 19:42:30 +0530

    constructor injection

--- SpringDemo/src/com/rohuu/drawingApp/Triangle.java
+++ SpringDemo/src/com/rohuu/drawingApp/Triangle.java
@@ -3,7 +3,25 @@ package com.rohuu.drawingApp;
 public class Triangle {
 	
 	private String type;
+	private int height;
 	
+	public int getHeight() {
+		return height;
+	}
+
+
+	public Triangle(String type) {
+		this.type=type;
+	}
+	
+	public Triangle(int height) {
+		this.height=height;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type=type;
+		this.height=height;
+	}
 	
 	
 	public String getType() {
@@ -21,6 +39,6 @@ public class Triangle {
 
 
 	public void draw() {
-		System.out.println(getType() +" Triangle drawn");
+		System.out.println(getType() +" Triangle drawn of height "+getHeight());
 	}
 }
