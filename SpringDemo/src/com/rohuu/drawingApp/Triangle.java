a862ddbbd7d95f3ec7bac701db5b27a343cd4ef7
Rohuu <rohuu99022@gmail.com>
2023-03-22 20:05:30 +0530

    initializing list via xml configuration using list tag

--- SpringDemo/src/com/rohuu/drawingApp/Triangle.java
+++ SpringDemo/src/com/rohuu/drawingApp/Triangle.java
@@ -6,21 +6,17 @@ public class Triangle {
 
 	List<Point> points;
 
-
 	public List<Point> getPoints() {
 		return points;
 	}
-
 
 	public void setPoints(List<Point> points) {
 		this.points = points;
 	}
 
-
 	public void draw() {
-		
-		for(Point point:points) {
-			System.out.println("Point = ("+point.getX()+ ", " +point.getY()+")");
+		for(Point point: points) {
+		System.out.println("Point = ("+point.getX()+ ", " +point.getY()+")");
 		}
 	}
 }
