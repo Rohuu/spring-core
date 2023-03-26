279362d632f935a0c5894dd6fec4010595c51f00
Rohuu <rohuu99022@gmail.com>
2023-03-26 12:19:54 +0530

    @Service annotation

--- SpringDemo/src/com/rohuu/drawingApp/Circle.java
+++ SpringDemo/src/com/rohuu/drawingApp/Circle.java
@@ -7,8 +7,9 @@ import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class Circle implements Shape {
 	
 	private Point center;
